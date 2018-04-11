7e17ab5ac38c652c5fdf0964085e4bbd8fc7a361
jojojonah <athomasjonah@gmail.com>
2018-04-10 23:17:25 -0700

    Update local.js

--- local.js
+++ local.js
@@ -1,36 +1,162 @@
+// CLOCK
 function clock() {
  var date = new Date(),
-     hours = date.getHours(),
-     minutes = date.getMinutes(),
+     month,
+     year,
+     day,
+     hour,
+     minute,
      meridiem,
-     am = hours <= 12,
-     pm = hours >= 12,
-     time;
+     am,
+     pm,
+     time,
+     desktop;
   
-  // var time = (hours == 0 ? 12 : hours) + ':' 
-  //          + (minutes < 10 ? '0' + minutes : minutes) 
-  //          + (pm ? ' PM' : ' AM');
-  
-  if (hours == 0) {
-    hours = 12;
+  function characterVariationsDestinction() {
+    desktop = $('.time').parents('.desktop').length;
   }
+  characterVariationsDestinction();
   
-  if (minutes.length == 1) {
-    minutes = '000' + minutes;
+  function monthConversions() {
+    month = date.getMonth();
+
+      if (month == 0) {
+        month = 'January';
+      }
+
+      if (month == 1) {
+        month = 'February';
+      }
+
+      if (month == 2) {
+        month = 'March';
+      }
+
+      if (month == 3) {
+        month = 'April';
+      }
+
+      if (month == 4) {
+        month = 'May';
+      }
+
+      if (month == 5) {
+        month = 'June';
+      }
+
+      if (month == 6) {
+        month = 'July';
+      } 
+      
+      if (month == 7) {
+        month = 'August';
+      } 
+      
+      if (month == 8) {
+        month = 'September';
+      } 
+      
+      if (month == 9) {
+        month = 'October';
+      } 
+      
+      if (month == 10) {
+        month = 'November';
+      } 
+      
+      if (month == 11) {
+        month = 'December';
+      } 
   }
+  monthConversions();
   
-  if (am) {
-    meridiem = 'AM';
+  function yearConversions() {
+    year = date.getYear();
   }
-  
-  if (pm) {
-    hours -= 12;
-    meridiem = 'PM';
+  yearConversions();
+   
+  function dayConversions() {
+    day = date.getDay();
+    
+    if (desktop) {
+      if (day == 0) {
+        day = 'Sun';
+      }
+
+      if (day == 1) {
+        day = 'Mon';
+      }
+
+      if (day == 2) {
+        day = 'Tue';
+      }
+
+      if (day == 3) {
+        day = 'Wed';
+      }
+
+      if (day == 4) {
+        day = 'Thu';
+      }
+
+      if (day == 5) {
+        day = 'Fri';
+      }
+
+      if (day == 6) {
+        day = 'Sat';
+      } 
+    }
   }
+  dayConversions();
   
-  time = hours + ':' + minutes + ' ' + meridiem;
+  function hourConversions() {
+    hour = date.getHours();
+    
+    if (hour == 0) {
+      hour = 12;
+    }
+    
+    if (hour <= 12) {
+      hour -= 12;
+    }
+  }
+  hourConversions();
   
-  $('.time')[0].innerHTML = time;
+  function minuteConversions() {
+    minute = date.getMinutes();
+    
+    if (desktop) {
+      if (minute < 10) {
+        minute = '0' + minute;
+}
+    }
+  }
+  minuteConversions();
+  
+  function meridiemConversions() {
+    am = hour <= 12,
+    pm = hour >= 12;
+    
+    if (am) {
+      meridiem = 'AM';
+    }
+
+    if (pm) {
+      hour -= 12;
+      meridiem = 'PM';
+    }
+  }
+  meridiemConversions();
+  
+  function placements() {
+    if (desktop) {
+      time = day + ' ' + hour + ':' + minute + ' ' + meridiem;
+      
+      $('.desktop .menu-bar .time')[0].innerHTML = time;
+    }
+  }
+  placements();
 }
 
 
